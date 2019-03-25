9cce4e67f8b7ab2caf822b8c3dfa2108c78f6c23
PanGeun <lifebook0809@gmail.com>
2019-03-25 20:22:53 +0900

    make first bootstrap on likelion

---
+++ test.py
@@ -0,0 +1,13 @@
+
+# test_list = ['one', 'two', 'three']
+# for i in test_list:
+#     print(i)
+
+lion_we_know = ['hyojin', 'seol', 'eunjin', 'kijung']
+lion_meet = ['hyojin', 'seol', 'bak']
+
+for lion in lion_meet:
+    if lion in lion_we_know:
+        print("hello!")
+    else:
+        print("I don't know")
